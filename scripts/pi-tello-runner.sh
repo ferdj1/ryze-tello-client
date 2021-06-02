feca364b451a758066cbe0e6968d5faae0116ca0
Dominik Juric <djuric@croz.net>
2021-06-02 23:57:43 +0200

    RyzeTello Communicator

--- scripts/pi-tello-runner.sh
+++ scripts/pi-tello-runner.sh
@@ -5,44 +5,48 @@ export RYZE_TELLO_DRONE_HOST=192.168.10.1
 export RYZE_TELLO_DRONE_PORT=8889
 export RYZE_TELLO_DRONE_VIDEO_PORT=11111
 
-# Try to connect to drone network
 while true; do
-    ping -c1 -q 192.168.10.1
-    droneNetworkStatus=$?;
 
-    if [ $droneNetworkStatus -eq 0 ]; then
-        break
-    else
-        # Backup link for development purposes
-        ping -c1 -q 192.168.100.1
-        homeNetworkStatus=$?;
+  # Try to connect to drone network
+  while true; do
+      ping -c1 -q 192.168.10.1
+      droneNetworkStatus=$?;
 
-        if [ $homeNetworkStatus -eq 0 ]; then
-            break
-        fi
+      if [ $droneNetworkStatus -eq 0 ]; then
+          break
+      else
+          # Backup link for development purposes
+          ping -c1 -q 192.168.100.1
+          homeNetworkStatus=$?;
 
-        # not connected, sleeping for a second
-        sleep 1
-    fi
+          if [ $homeNetworkStatus -eq 0 ]; then
+              break
+          fi
+
+          # not connected, sleeping for a second
+          sleep 1
+      fi
+
+  done
+
+  # Try to connect to internet and coordinator server via 4G
+  sakis3g connect &
+
+  sleep 10
+  while true; do
+      ping -c1 -q 40.115.62.3
+      internetConnectionStatus=$?;
+
+      if [ $internetConnectionStatus -eq 0 ]; then
+          break
+      else
+          # not connected, sleeping for a second
+          sleep 1
+      fi
+  done
+
+  # Connected to both internet and drone AP, run communicator
+  cd /home/pi/ryze-tello/
+  java my.project.fer.ryzetello.client.RyzeTelloRaspberryPiTcpClient
 
 done
-
-# Try to connect to internet and coordinator server via 4G
-sakis3g connect &
-
-sleep 10
-while true; do
-    ping -c1 -q 40.115.62.3
-    internetConnectionStatus=$?;
-
-    if [ $internetConnectionStatus -eq 0 ]; then
-        break
-    else
-        # not connected, sleeping for a second
-        sleep 1
-    fi
-done
-
-# Connected to both internet and drone AP, run communicator
-cd /home/pi/ryze-tello/
-java my.project.fer.ryzetello.client.RyzeTelloRaspberryPiTcpClient &
